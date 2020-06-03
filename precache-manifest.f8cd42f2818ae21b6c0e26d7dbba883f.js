29d7c004b912b9b2b8ba5eed4ccff942ddc898b8
imanol <imanol.rodriguez@fhios.es>
2020-06-03 15:27:32 +0200

    Updates

---
+++ precache-manifest.f8cd42f2818ae21b6c0e26d7dbba883f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ab60a12e076e69377fe9e86c0d0759",
+    "url": "/BookReader/index.html"
+  },
+  {
+    "revision": "85f477476c21b7988a84",
+    "url": "/BookReader/static/js/2.6a33a7e3.chunk.js"
+  },
+  {
+    "revision": "81d9b24e0b22758e373574b225b8549b",
+    "url": "/BookReader/static/js/2.6a33a7e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad25e32c3ea8bf829c78",
+    "url": "/BookReader/static/js/main.121a2561.chunk.js"
+  },
+  {
+    "revision": "b58b59e5800fa0b393cc",
+    "url": "/BookReader/static/js/runtime-main.e4e16ab0.js"
+  }
+]);
